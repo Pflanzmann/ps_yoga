6b6b44f3a2c3fae4b9adb419e25a0c17dcab844a
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-06-07 16:25:49 +0200

    authenticate and send message

--- Assets/Requests/Scripts/NetworkManager.cs
+++ Assets/Requests/Scripts/NetworkManager.cs
@@ -7,57 +7,122 @@ using UnityEngine.Networking;
 
 public class NetworkManager : MonoBehaviour
 {
-    public void sendRequest()
+    // gernerated tokens or from file 
+    private String accessToken = null;
+    private String refreshToken = null;
+
+    // client credentials
+    private readonly String clientID = "ni4yBfTfQiq3rV5jBLYuDQ";
+    private readonly String clientSecret = "GDNIKhxdPC90qt9Zy8Bj3AMH1dBs8j1p";
+    private String stateOfAccount;
+    // ngrok server uri to forward to localhost
+    private readonly String redirectUri = "https://2551-84-170-79-12.eu.ngrok.io";
+    // send message body data
+    public String message = "test";
+    public String to_contact = "felix.kunath@web.de";
+    // for getting code to get token
+    private String authCode = null;
+    private bool gotToken = false;
+
+    // private String authUrl = "https://zoom.us/oauth/authorize?response_type=code&client_id=Lge3pEcAT3aSoNxRolGnFw&redirect_uri=https://example.com";
+
+    public void SendRequest()
     {
-        StartCoroutine(MakeRequests());
+        
+        //JSONWriter jsonWriter = new JSONWriter();
+        //jsonWriter.WriteToken("abc","def");
+        //jsonWriter.ReadTokens();
+
+        StartCoroutine(GetToken());
+        StartCoroutine(SendMessage());
+    }
+    public void SendYogaResult()
+    {
+        if (this.accessToken == null)
+            StartCoroutine(GetToken());
+
+        StartCoroutine(SendMessage());
     }
 
-    private IEnumerator MakeRequests()
+    private IEnumerator SendMessage()
     {
-        // GET
-        var getRequest = CreateRequest("https://jsonplaceholder.typicode.com/todos/1");
-        // AttachHeader(getRequest, "Authorization", "bearer eeuirhiu23hr2r8934hr983");
-        yield return getRequest.SendWebRequest();
-        var deserializedGetData = JsonUtility.FromJson<Todo>(getRequest.downloadHandler.text);
-        print(deserializedGetData.title);
-
-        // GET List Users
-        var postSendMesData = new SendMessageData() { message = "hallo", to_contact = "felix.kunath@web.de" };
+        while (!this.gotToken)
+        {
+            yield return new WaitForSeconds(2);
+        }
+        // POST send message after Bearer enter your OAuthToken
+        // to_contact email has to be in your contacts
+        var postSendMesData = new SendMessageData() { message = this.message, to_contact = this.to_contact };
         var postReqSendMessage = CreateRequest("https://api.zoom.us/v2/chat/users/me/messages", RequestType.POST, postSendMesData);
-        AttachHeader(postReqSendMessage,"Authorization", "Bearer eyJhbGciOiJIUzUxMiIsInYiOiIyLjAiLCJraWQiOiI2ZjgxMmIxMS00ZWIzLTRmNDMtYjJiYS03ODUyMGM5MTFmYzQifQ.eyJ2ZXIiOjcsImF1aWQiOiJmNGNhNGYyZGM1MzJhODJiMzJiMzQ0ZjI1NDQ1YTczYSIsImNvZGUiOiJLTlBiQzk2OFBjX3lKdy1mMGozUnphX0ZZYmhXR0I4UWciLCJpc3MiOiJ6bTpjaWQ6TkZBREJrRXlTUUdQMThaY002aUZEUSIsImdubyI6MCwidHlwZSI6MCwidGlkIjowLCJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJ5SnctZjBqM1J6YV9GWWJoV0dCOFFnIiwibmJmIjoxNjUzMzIyOTM3LCJleHAiOjE2NTMzMjY1MzcsImlhdCI6MTY1MzMyMjkzNywiYWlkIjoiT3JIOXBTb29TcE9xMHNtNDlBX1U0QSIsImp0aSI6Ijc5M2M3Y2E2LWFjNjktNDhmNy04ODQ1LWZjNmQ5NjRiYTY1NiJ9.uk3Soc9GbMPigSCj6er7jGoGtKFF7wVxB5v2EAd0UHYkoXVwtyvVGekOAQAi7jR6yBdy9Ulf9NulrCUAmc0C8Q");
-        //print(postReqSendMessage.GetRequestHeader);
+        AttachHeader(postReqSendMessage, "Authorization", "Bearer " + this.accessToken);
         yield return postReqSendMessage.SendWebRequest();
-        var deserializedPostData1 = JsonUtility.FromJson<PostResult>(postReqSendMessage.downloadHandler.text);
-
-        // POST
-        var dataToPost = new PostData() { Hero = "John Wick", PowerLevel = 9001 };
-        var postRequest = CreateRequest("https://reqbin.com/echo/post/json", RequestType.POST, dataToPost);
-        yield return postRequest.SendWebRequest();
-        var deserializedPostData = JsonUtility.FromJson<PostResult>(postRequest.downloadHandler.text);
-        // Trigger continuation of game flow
+        // wenn ergebnis nicht id: ....
+        print("POST DATA1 " + postReqSendMessage.downloadHandler.text);
     }
 
+    private IEnumerator GetToken()
+    {
+        this.stateOfAccount = System.Guid.NewGuid().ToString();
+        Application.OpenURL("https://zoom.us/oauth/authorize?response_type=code&client_id=" + this.clientID + "&redirect_uri=" + this.redirectUri + "&state=" + this.stateOfAccount);
 
-    private UnityWebRequest CreateRequest(string path, RequestType type = RequestType.POST, object data = null)
+        while (this.authCode == null || this.authCode.Equals("error no code found for given state"))
+        {
+            yield return new WaitForSeconds(5f);
+            var getCode = CreateRequest(redirectUri + "/code/" + this.stateOfAccount, RequestType.GET);
+            yield return getCode.SendWebRequest();
+            this.authCode = getCode.downloadHandler.text;
+            print(authCode);
+        }
+        
+        // Post get oAuth 2.0 access and refresh token
+        String clientIdAndSecret = EncodeTo64(clientID + ":" + clientSecret);
+        print(clientIdAndSecret);
+        var postToken = CreateRequest("https://zoom.us/oauth/token?code=" + this.authCode + "&grant_type=authorization_code&redirect_uri=" + this.redirectUri, RequestType.POST);
+        AttachHeader(postToken, "Authorization", "Basic " + clientIdAndSecret);
+        yield return postToken.SendWebRequest();
+        var deserializedPostData = JsonUtility.FromJson<PostGetToken>(postToken.downloadHandler.text);
+        this.accessToken = deserializedPostData.access_token;
+        this.refreshToken = deserializedPostData.refresh_token;
+        print("Token Data:  " + postToken.downloadHandler.text);
+
+        this.gotToken = true;
+
+    }
+
+    private UnityWebRequest CreateRequest(string path, RequestType type, object data = null)
     {
         var request = new UnityWebRequest(path, type.ToString());
 
         if (data != null)
         {
             var bodyRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(data));
-            print("PostBody"+ bodyRaw);
+            //print("PostBody"+ bodyRaw);
             request.uploadHandler = new UploadHandlerRaw(bodyRaw);
         }
 
         request.downloadHandler = new DownloadHandlerBuffer();
+        if (path.Equals("https://zoom.us/oauth/token"))
+        {
+            print("getToken Post");
+            request.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            return request;
+        }
         request.SetRequestHeader("Content-Type", "application/json");
-
         return request;
     }
 
     private void AttachHeader(UnityWebRequest request, string key, string value)
     {
         request.SetRequestHeader(key, value);
+    }
+
+    static public string EncodeTo64(string toEncode)
+    {
+        byte[] toEncodeAsBytes
+              = Encoding.ASCII.GetBytes(toEncode);
+        string returnValue
+              = Convert.ToBase64String(toEncodeAsBytes);
+        return returnValue;
     }
 }
 
@@ -80,16 +145,16 @@ public class Todo
 }
 
 [Serializable]
-public class PostData
-{
-    public string Hero;
-    public int PowerLevel;
-}
-[Serializable]
 public class SendMessageData
 {
     public string message;
     public string to_contact;
+}
+[Serializable]
+public class PostGetToken
+{
+    public string access_token;
+    public String refresh_token;
 }
 
 public class PostResult
