a7fc69f19f00852625271d2a9df1a7393171a598
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-06-07 16:25:49 +0200

    authenticate and send message

---
+++ Assets/Requests/Scripts/TokenClassName.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TokenClassName : MonoBehaviour
+{
+    [Serializable]
+    public class TokenName
+    {
+        public string access_token;
+    }
+
+    private static IEnumerator GetAccessToken(Action<string> result)
+    {
+        Dictionary<string, string> content = new Dictionary<string, string>();
+        //Fill key and value
+        content.Add("grant_type", "client_credentials");
+        content.Add("client_id", "login-secret");
+        content.Add("client_secret", "secretpassword");
+
+        UnityWebRequest www = UnityWebRequest.Post("https://someurl.com//oauth/token", content);
+        //Send request
+        yield return www.Send();
+
+        if (!www.isNetworkError)
+        {
+            string resultContent = www.downloadHandler.text;
+            TokenName json = JsonUtility.FromJson<TokenName>(resultContent);
+
+            //Return result
+            result(json.access_token);
+        }
+        else
+        {
+            //Return null
+            result("");
+        }
+    }
+}
