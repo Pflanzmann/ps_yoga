85e7882a24c2b4c6b23819fce36627b95a2f4c55
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-06-18 18:06:20 +0200

    change pose after given duration

---
+++ Assets/PoseManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> poseList;
+    [SerializeField] private float poseDuration;
+    
+    private float timer;
+    private int currentPoseIndex = 0;
+
+    private void Start()
+    {
+        foreach (var pose in poseList)
+        {
+            pose.SetActive(false);
+        }   
+        
+        poseList[0].SetActive(true);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer < poseDuration)
+        {
+            return;
+        }
+        timer = 0;
+        
+        StartNextPose();
+    }
+
+    private void StartNextPose()
+    {
+        foreach (var pose in poseList)
+        {
+            pose.SetActive(false);
+        }   
+        
+        currentPoseIndex += 1;
+        poseList[currentPoseIndex].SetActive(true);
+    }
+    
+}
