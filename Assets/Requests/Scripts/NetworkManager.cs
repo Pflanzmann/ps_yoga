262f676e90d8f5f66c1b900d071dac76efae37cc
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-06-18 20:29:40 +0200

    Update NetworkManager.cs
    - network manager gets indirect from other server than zoom server

--- Assets/Requests/Scripts/NetworkManager.cs
+++ Assets/Requests/Scripts/NetworkManager.cs
@@ -12,21 +12,22 @@ public class NetworkManager : MonoBehaviour
     private String refreshToken = null;
 
     // client credentials
-    private readonly String clientID = "ni4yBfTfQiq3rV5jBLYuDQ";
-    private readonly String clientSecret = "GDNIKhxdPC90qt9Zy8Bj3AMH1dBs8j1p";
+    private readonly String clientID = "dvdxc0Z2SsWaOebq32qlvA";
+    private readonly String clientSecret = "u8Wmy1Emv3p1V1ieP68GPjmDgvyVdJuV";
     private String stateOfAccount;
     // ngrok server uri to forward to localhost
-    private readonly String redirectUri = "https://2551-84-170-79-12.eu.ngrok.io";
+    private readonly String redirectUri = "https://bb5e-2a00-20-6041-c0c1-d98c-a8a4-ea1d-a59f.eu.ngrok.io";
     // send message body data
-    public String message = "test";
-    public String to_contact = "felix.kunath@web.de";
+    public String message = "hallo";
+    public String to_contact = "timo.ji317@web.de";
     // for getting code to get token
     private String authCode = null;
     private bool gotToken = false;
+    private bool isUrlOpen = false;
 
     public void Start()
     {
-        
+        this.to_contact = "timo.ji317@web.de";
     }
     public void SendRequest()
     {
@@ -42,35 +43,13 @@ public class NetworkManager : MonoBehaviour
 
     public void SendYogaResult()
     {
-        if (this.accessToken == null) {
-            if (this.refreshToken == null)
-                StartCoroutine(GetToken());
-            else
-                StartCoroutine(RefreshToken());
+        if (this.accessToken == null)
+        {
+            StartCoroutine(GetToken());
         }
         else
             this.gotToken = true;
-
-        if (this.to_contact == null)
-            StartCoroutine(GetHost());
-        StartCoroutine(SendMessage());
-    }
-
-    private string GetHost()
-    {
-        throw new NotImplementedException();
-    }
-
-    private IEnumerator RefreshToken()
-    {
-        String clientIdAndSecret = EncodeTo64(clientID + ":" + clientSecret);
-        var postRefreshToken = CreateRequest("https://zoom.us/oauth/token?refresh_token=" + this.refreshToken + "&grant_type=refresh_token", RequestType.POST);
-        AttachHeader(postRefreshToken, "Authorization", "Basic " + clientIdAndSecret);
-        yield return postRefreshToken.SendWebRequest();
-        var deserializedPostData = JsonUtility.FromJson<PostGetToken>(postRefreshToken.downloadHandler.text);
-        this.accessToken = deserializedPostData.access_token;
-        this.refreshToken = deserializedPostData.refresh_token;
-        print("Token Data:  " + postRefreshToken.downloadHandler.text);
+            StartCoroutine(SendMessage());
     }
 
     private IEnumerator SendMessage()
@@ -79,41 +58,66 @@ public class NetworkManager : MonoBehaviour
         {
             yield return new WaitForSeconds(2);
         }
-        // POST send message after Bearer enter your OAuthToken
+        // POST send message
+        // after Bearer enter your OAuthToken
         // to_contact email has to be in your contacts
         var postSendMesData = new SendMessageData() { message = this.message, to_contact = this.to_contact };
+        print(this.to_contact);
         var postReqSendMessage = CreateRequest("https://api.zoom.us/v2/chat/users/me/messages", RequestType.POST, postSendMesData);
         AttachHeader(postReqSendMessage, "Authorization", "Bearer " + this.accessToken);
         yield return postReqSendMessage.SendWebRequest();
-
+        if (postReqSendMessage.responseCode == 201)
+            print("no error");
         // wenn ergebnis nicht "id: ...." dann fehlerbehandlung -> access token falsch => refresh token
         print("POST DATA1 " + postReqSendMessage.downloadHandler.text);
     }
 
     private IEnumerator GetToken()
     {
-        this.stateOfAccount = System.Guid.NewGuid().ToString();
-        Application.OpenURL("https://zoom.us/oauth/authorize?response_type=code&client_id=" + this.clientID + "&redirect_uri=" + this.redirectUri + "&state=" + this.stateOfAccount);
-
-        while (this.authCode == null || this.authCode.Equals("error no code found for given state"))
+     
+        //yield return new WaitForSeconds(100f);
+        // while the server doesn't answer with an code
+        // retry every 5 seconds to get code
+        //while (this.authCode == null || this.authCode.Equals("error no code found for given state"))
+        //{
+        //    yield return new WaitForSeconds(5f);
+        //    var getCode = CreateRequest(redirectUri + "/code/" + this.stateOfAccount, RequestType.GET);
+        //    yield return getCode.SendWebRequest();
+        //    // when server not accesible
+        //    // and ngrok gives an error in html format
+        //    // dont set authCode to html output
+        //    if(!getCode.downloadHandler.text.Contains("<"))
+        //        this.authCode = getCode.downloadHandler.text;
+        //}
+        while (this.accessToken == null || this.accessToken.Equals("NO_TOKEN"))
         {
-            yield return new WaitForSeconds(5f);
-            var getCode = CreateRequest(redirectUri + "/code/" + this.stateOfAccount, RequestType.GET);
+            yield return new WaitForSeconds(2f);
+            var getCode = CreateRequest(redirectUri + "/token", RequestType.GET);
             yield return getCode.SendWebRequest();
-            this.authCode = getCode.downloadHandler.text;
-            print(authCode);
-        }
-        
-        // Post get oAuth 2.0 access and refresh token
-        String clientIdAndSecret = EncodeTo64(clientID + ":" + clientSecret);
-        var postToken = CreateRequest("https://zoom.us/oauth/token?code=" + this.authCode + "&grant_type=authorization_code&redirect_uri=" + this.redirectUri, RequestType.POST);
-        AttachHeader(postToken, "Authorization", "Basic " + clientIdAndSecret);
-        yield return postToken.SendWebRequest();
-        var deserializedPostData = JsonUtility.FromJson<PostGetToken>(postToken.downloadHandler.text);
-        this.accessToken = deserializedPostData.access_token;
-        this.refreshToken = deserializedPostData.refresh_token;
-        print("Token Data:  " + postToken.downloadHandler.text);
+            // when server not accesible
+            // and ngrok gives an error in html format
+            // dont set authCode to html output
+            if (getCode.downloadHandler.text.Equals("NO_TOKEN") && !isUrlOpen){
 
+                this.stateOfAccount = System.Guid.NewGuid().ToString();
+                Application.OpenURL("https://zoom.us/oauth/authorize?response_type=code&client_id=" + this.clientID + "&redirect_uri=" + this.redirectUri + "&state=" + this.stateOfAccount);
+                this.isUrlOpen = true;
+            }
+            if (!getCode.downloadHandler.text.Contains("<"))
+                this.accessToken = getCode.downloadHandler.text;
+        }
+        this.isUrlOpen = false;
+
+        // Post get oAuth 2.0 access and refresh token
+        //String clientIdAndSecret = EncodeTo64(clientID + ":" + clientSecret);
+        //var postToken = CreateRequest("https://zoom.us/oauth/token?code=" + this.authCode + "&grant_type=authorization_code&redirect_uri=" + this.redirectUri, RequestType.POST);
+        //AttachHeader(postToken, "Authorization", "Basic " + clientIdAndSecret);
+        //yield return postToken.SendWebRequest();
+        //var deserializedPostData = JsonUtility.FromJson<PostGetToken>(postToken.downloadHandler.text);
+        //this.accessToken = deserializedPostData.access_token;
+        //this.refreshToken = deserializedPostData.refresh_token;
+        //print("Token Data:  " + postToken.downloadHandler.text);
+        print(this.accessToken);
         this.gotToken = true;
 
     }
