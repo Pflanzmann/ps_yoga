f2cc60b1e81af2a11514ad9bfc1e69d068dc2a8a
daotrung2497 <ductrungcsp@gmail.com>
2022-06-20 12:10:55 +0200

    changed postevaluationmanager

--- Assets/Scripts/PoseEvaluationManager.cs
+++ Assets/Scripts/PoseEvaluationManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿/*using System.Collections.Generic;
 
 public class PoseEvaluationManager : BaseGameEventListener<PoseData> {
 
@@ -6,16 +6,26 @@ public class PoseEvaluationManager : BaseGameEventListener<PoseData> {
     public override void OnEventRaised(PoseData value) {
         base.OnEventRaised(value);
         foreach(var jointData in value.JointDatas) {
-            print("JoinType: " + jointData.JointType + " | ErrorValue: " + jointData.ErrorValue + " | IsCorrect: " + jointData.IsCorrect);
+            //print("JoinType: " + jointData.JointType + " | ErrorValue: " + jointData.ErrorValue + " | IsCorrect: " + jointData.IsCorrect);
+            if(jointData.IsCorrect == false)
+            {
+                print("false Pose: " + jointData.JointType);
+            }
+            
         }
 
         //var ausgewerteterStreing = evaluate(value);
 
         //dataSuccessfullyEvaluatedEvent.Raise(ausgewerteterStreing);
+        //evaluate(value);
+        //print("sdonl" );
     }
+    
 
-    private void evaluate(PoseData data) {
+
+    /*private void evaluate(PoseData data) {
         //magic
+        base.OnEventRaised(data);
         List<JointData> magic = new List<JointData>();
         foreach (var jointData in data.JointDatas)
         {
@@ -24,8 +34,48 @@ public class PoseEvaluationManager : BaseGameEventListener<PoseData> {
                 magic.Add(jointData);
             }
         }
-        print("All falsh jointtype: " + magic);
+        print("All falsh jointtype: " );
+        //return "abc";
+    }*/
+    
+//}
+
+using System.Collections.Generic;
+public class PoseEvaluationManager : BaseGameEventListener<PoseData> {
+
+
+    public override void OnEventRaised(PoseData value) {
+        base.OnEventRaised(value);
+        foreach(var jointData in value.JointDatas) {
+            print("JoinType: " + jointData.JointType + " | ErrorValue: " + jointData.ErrorValue + " | IsCorrect: " + jointData.IsCorrect);
+              
         
+
+        //var ausgewerteterStreing = evaluate(value);
+
+        //dataSuccessfullyEvaluatedEvent.Raise(ausgewerteterStreing);
+        //print(ausgewerteterStreing);
+        print(evaluate(value));
+        }
+       }
+
+    private string evaluate(PoseData value) {
+        //magic
+        var magic=new List<JointData>();
+        foreach(var jointData in value.JointDatas)
+        {
+            if(jointData.IsCorrect == false)
+            {
+                magic.Add(jointData);
+            }
+        }
+        var allFalschPose = " All false Poses : ";
+        foreach (var falschepose in magic)
+        {
+            allFalschPose += falschepose.JointType + " ,";
+        }
+        allFalschPose += " .";
+        return allFalschPose;
     }
 
 }
