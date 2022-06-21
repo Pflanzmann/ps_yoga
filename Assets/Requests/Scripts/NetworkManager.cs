41891e062127c3040489e43059cd08601a86c3c5
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-06-21 23:06:55 +0200

    networkmanager improvements
    - deleted c# file writer class
    - oauth server is asked for token if current token is invalid

--- Assets/Requests/Scripts/NetworkManager.cs
+++ Assets/Requests/Scripts/NetworkManager.cs
@@ -9,33 +9,25 @@ public class NetworkManager : MonoBehaviour
 {
     // gernerated tokens or from file 
     private String accessToken = null;
-    private String refreshToken = null;
-
     // client credentials
     private readonly String clientID = "dvdxc0Z2SsWaOebq32qlvA";
-    private readonly String clientSecret = "u8Wmy1Emv3p1V1ieP68GPjmDgvyVdJuV";
     private String stateOfAccount;
     // ngrok server uri to forward to localhost
-    private readonly String redirectUri = "https://bb5e-2a00-20-6041-c0c1-d98c-a8a4-ea1d-a59f.eu.ngrok.io";
+    private readonly String redirectUri = "https://5170-2003-ea-1717-fb27-f436-c95-412b-212e.eu.ngrok.io";
     // send message body data
     public String message = "hallo";
     public String to_contact = "timo.ji317@web.de";
     // for getting code to get token
-    private String authCode = null;
     private bool gotToken = false;
     private bool isUrlOpen = false;
 
     public void Start()
     {
         this.to_contact = "timo.ji317@web.de";
+        this.message = "Hi wie gehts?";
     }
     public void SendRequest()
     {
-        
-        //JSONWriter jsonWriter = new JSONWriter();
-        //jsonWriter.WriteToken("abc","def");
-        //jsonWriter.ReadTokens();
-
         StartCoroutine(GetToken());
         StartCoroutine(SendMessage());
     }
@@ -48,12 +40,16 @@ public class NetworkManager : MonoBehaviour
             StartCoroutine(GetToken());
         }
         else
+        {
             this.gotToken = true;
-            StartCoroutine(SendMessage());
+        }
+        StartCoroutine(SendMessage());
+
     }
 
     private IEnumerator SendMessage()
     {
+        // timeout after certain amount of tries
         while (!this.gotToken)
         {
             yield return new WaitForSeconds(2);
@@ -64,31 +60,27 @@ public class NetworkManager : MonoBehaviour
         var postSendMesData = new SendMessageData() { message = this.message, to_contact = this.to_contact };
         print(this.to_contact);
         var postReqSendMessage = CreateRequest("https://api.zoom.us/v2/chat/users/me/messages", RequestType.POST, postSendMesData);
+        print(this.accessToken);
         AttachHeader(postReqSendMessage, "Authorization", "Bearer " + this.accessToken);
         yield return postReqSendMessage.SendWebRequest();
         if (postReqSendMessage.responseCode == 201)
+        {
             print("no error");
+        }
+        else
+        {
+            // retry with and get new token
+            this.gotToken = false;
+            this.accessToken = null;
+            SendYogaResult();
+            print("error" + postReqSendMessage.downloadHandler.error);
+        }
         // wenn ergebnis nicht "id: ...." dann fehlerbehandlung -> access token falsch => refresh token
         print("POST DATA1 " + postReqSendMessage.downloadHandler.text);
     }
 
     private IEnumerator GetToken()
     {
-     
-        //yield return new WaitForSeconds(100f);
-        // while the server doesn't answer with an code
-        // retry every 5 seconds to get code
-        //while (this.authCode == null || this.authCode.Equals("error no code found for given state"))
-        //{
-        //    yield return new WaitForSeconds(5f);
-        //    var getCode = CreateRequest(redirectUri + "/code/" + this.stateOfAccount, RequestType.GET);
-        //    yield return getCode.SendWebRequest();
-        //    // when server not accesible
-        //    // and ngrok gives an error in html format
-        //    // dont set authCode to html output
-        //    if(!getCode.downloadHandler.text.Contains("<"))
-        //        this.authCode = getCode.downloadHandler.text;
-        //}
         while (this.accessToken == null || this.accessToken.Equals("NO_TOKEN"))
         {
             yield return new WaitForSeconds(2f);
@@ -103,20 +95,12 @@ public class NetworkManager : MonoBehaviour
                 Application.OpenURL("https://zoom.us/oauth/authorize?response_type=code&client_id=" + this.clientID + "&redirect_uri=" + this.redirectUri + "&state=" + this.stateOfAccount);
                 this.isUrlOpen = true;
             }
+            // error between ngrok and server for tokens
+            // response is html
             if (!getCode.downloadHandler.text.Contains("<"))
                 this.accessToken = getCode.downloadHandler.text;
-        }
+        } 
         this.isUrlOpen = false;
-
-        // Post get oAuth 2.0 access and refresh token
-        //String clientIdAndSecret = EncodeTo64(clientID + ":" + clientSecret);
-        //var postToken = CreateRequest("https://zoom.us/oauth/token?code=" + this.authCode + "&grant_type=authorization_code&redirect_uri=" + this.redirectUri, RequestType.POST);
-        //AttachHeader(postToken, "Authorization", "Basic " + clientIdAndSecret);
-        //yield return postToken.SendWebRequest();
-        //var deserializedPostData = JsonUtility.FromJson<PostGetToken>(postToken.downloadHandler.text);
-        //this.accessToken = deserializedPostData.access_token;
-        //this.refreshToken = deserializedPostData.refresh_token;
-        //print("Token Data:  " + postToken.downloadHandler.text);
         print(this.accessToken);
         this.gotToken = true;
 
@@ -129,7 +113,6 @@ public class NetworkManager : MonoBehaviour
         if (data != null)
         {
             var bodyRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(data));
-            //print("PostBody"+ bodyRaw);
             request.uploadHandler = new UploadHandlerRaw(bodyRaw);
         }
 
@@ -147,15 +130,6 @@ public class NetworkManager : MonoBehaviour
     private void AttachHeader(UnityWebRequest request, string key, string value)
     {
         request.SetRequestHeader(key, value);
-    }
-
-    static public string EncodeTo64(string toEncode)
-    {
-        byte[] toEncodeAsBytes
-              = Encoding.ASCII.GetBytes(toEncode);
-        string returnValue
-              = Convert.ToBase64String(toEncodeAsBytes);
-        return returnValue;
     }
 }
 
@@ -177,9 +151,4 @@ public class PostGetToken
 {
     public string access_token;
     public String refresh_token;
-}
-
-public class PostResult
-{
-    public string success { get; set; }
 }
